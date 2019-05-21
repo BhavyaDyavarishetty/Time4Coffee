2f17bd5b51893c439003e75967703cee7dd49cf6
rajesh <rajesh@askmybraintrust.com>
2019-05-21 13:21:31 -0700

    added register

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Time4Coffee' });
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Time4Coffee2' });
 });
 
 module.exports = router;
